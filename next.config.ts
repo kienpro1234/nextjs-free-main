d3f973b387aec82c0fa385ceeb0c73fdc01fdf11
kienpro <nguyenduckien1109@gmail.com>
2024-12-22 09:42:25 +0700

    "update new code"

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "photo2.tinhte.vn",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
